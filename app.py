2d63c9037d83535615006f376bc983e997e750c1
Sarina <sarina@example.com>
2026-09-22 20:03:41 +0300

    Test automatic CI CD deployment

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return """
     <h1>Sarina - DevOps Task 2</h1>
-    <p>Deployed successfully with Dokku 🚀</p>
+    <p>Automatically deployed with GitHub Actions + Dokku 🚀</p>
     """
